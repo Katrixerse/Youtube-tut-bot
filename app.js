934c94bc96f131ffee6dbbf2fccf99689867ac8f
Zachary J. Myers <johnsmith97315@gmail.com>
2018-11-15 13:33:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,6 @@ Client.on("message", async (message) => {
    if (command === "announcement") {
 	   if (message.member.hasPermission("ADMINISTRATOR")) {
 		   const color = args[0]
-				
 		   const text = args.slice(1).join(" ");
 		   if (text.length < 1) return message.channel.send("Can not announce nothing");
 		   //const colour = args.slice(2).join("");
